e43fe8233e810212ec399b9a650a70a853036890
Victoria M <vmorris517@gmail.com>
2023-06-26 20:31:57 -0500

    added files

---
+++ src/main/java/org/launchcode/liftoffgroupproject/TaskManagerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoffgroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+
+}
