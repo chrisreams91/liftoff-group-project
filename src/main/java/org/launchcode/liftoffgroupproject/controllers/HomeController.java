8b0032ae1c2e54a97ceebb8446fbb5f81d6fbdc2
allybreeden <allybreeden@gmail.com>
2023-07-07 10:10:51 -0500

    Updated fragments.html with title, added bootstrap, and header (including navigation bar. Added fragments to index.html and cleaned up code

--- src/main/java/org/launchcode/liftoffgroupproject/controllers/HomeController.java
+++ src/main/java/org/launchcode/liftoffgroupproject/controllers/HomeController.java
@@ -1,14 +1,13 @@
 package org.launchcode.liftoffgroupproject.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("")
-    public String displayHomepage(Model model) {
+    public String displayHomepage() {
         return "index";
     }
 }
