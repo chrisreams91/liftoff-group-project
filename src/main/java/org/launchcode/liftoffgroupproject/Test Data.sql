507693c7803cb5b72cc5898cf570e5555e1ac982
sjesus <s.jesus10@yahoo.com>
2023-07-10 20:07:14 -0500

    SQL Queries

---
+++ src/main/java/org/launchcode/liftoffgroupproject/Test Data.sql
@@ -0,0 +1,29 @@
+INSERT INTO task
+VALUES (1, "feed dog before leaving for work", "12/19/2023", "Feed Dog", "2/19/2023");
+
+INSERT INTO task
+VALUES (2, "mob and sweep bedroom", "5/16/2016", "clean Bedroom", "6/19/2017");
+
+INSERT INTO task
+VALUES (3, "finish the work history section of my resume", "7/10/2023", "Update Resume", "7/4/2023");
+
+INSERT INTO task
+VALUES (4, "create the view for the login page", "7/29/2023", "Login Page View", "2/19/2023");
+
+INSERT INTO task
+VALUES (5, "Work on the controllers for the login page", "8/25/2023", "Login Page Controllers", "2/19/2023");
+
+INSERT INTO task
+VALUES (6, "add task button to the home page", "9/7/2023", "Add Task Button", "9/19/2023");
+
+INSERT INTO task
+VALUES (7, "Buy vegetables, cup noodles, and water", "9/9/2023", "Buy Groceries", "1/9/2023");
+
+INSERT INTO task
+VALUES (8, "Create sql queries for teammates to use as test data", "12/19/2023", "SQL Queries", "2/19/2023");
+
+INSERT INTO task
+VALUES (9, "Set alarm to 9:40 AM", "3/2/2023", "Set Alarm", "2/26/2023");
+
+INSERT INTO task
+VALUES (10, "order christmas gifts for dad and mom", "12/1/2023", "Christmas Gifts", "11/10/2023");
