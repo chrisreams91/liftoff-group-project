da62248e8b1af0929b6bae98a278fa5e823bc4ea
Shelvy <shelvy511@gmail.com>
2023-07-10 20:22:00 -0500

    update to List page Controller and list.html

--- src/main/java/org/launchcode/liftoffgroupproject/controllers/ListController.java
+++ src/main/java/org/launchcode/liftoffgroupproject/controllers/ListController.java
@@ -1,2 +1,26 @@
-package org.launchcode.liftoffgroupproject.controllers;public class ListController {
+package org.launchcode.liftoffgroupproject.controllers;
+
+import org.launchcode.liftoffgroupproject.data.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "list")
+public class ListController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+
+    @RequestMapping("")
+    public String list(Model model) {
+     model.addAttribute("tasks", taskRepository.findAll());
+     return "list";
+    }
 }
