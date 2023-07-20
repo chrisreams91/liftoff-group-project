8b418dde37009b5e358fdf95c7598caa5b60bcf5
allybreeden <allybreeden@gmail.com>
2023-07-20 18:54:16 -0500

    Updated HomeController, fixed "home" on navbar, and added path to Task button (to view all tasks)

--- src/main/java/org/launchcode/liftoffgroupproject/controllers/HomeController.java
+++ src/main/java/org/launchcode/liftoffgroupproject/controllers/HomeController.java
@@ -2,11 +2,13 @@ package org.launchcode.liftoffgroupproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("")
 public class HomeController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String displayHomepage() {
         return "index";
     }
