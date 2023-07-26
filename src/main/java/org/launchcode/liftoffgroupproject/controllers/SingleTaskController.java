9054040d1451e4387c162e3fc5bd454a873b1d36
kimsunhammond <kimsunhammond@gmail.com>
2023-07-26 11:20:45 -0500

    single task page modified on-click goes to single task

--- src/main/java/org/launchcode/liftoffgroupproject/controllers/SingleTaskController.java
+++ src/main/java/org/launchcode/liftoffgroupproject/controllers/SingleTaskController.java
@@ -1,11 +1,14 @@
 package org.launchcode.liftoffgroupproject.controllers;
 
+import org.launchcode.liftoffgroupproject.data.TaskRepository;
+import org.launchcode.liftoffgroupproject.models.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -13,36 +16,51 @@ import java.util.Optional;
 @RequestMapping("task")
 public class SingleTaskController {
 
-//    @Autowired
-//    private JobRepository jobRepository;
-//
-//    @RequestMapping("")
-//    public String index(Model model) {
-//
-//        model.addAttribute("title", "My Tasks");
-//        model.addAttribute("tasks", taskRepository.findAll());
-//
-//        return "index";
-//    }
+    @Autowired
+    private TaskRepository taskRepository;
 
-    @GetMapping("")
-    public String displaySingleTask() {
-        return "single-task";
-    }
-//}
-//    @GetMapping("view/{taskId}")
-//    public String displayViewTask(Model model, @PathVariable int taskId) {
-//        model.addAttribute("task", taskRepository.findById(taskId));
-////        return "view";
-//
-//
-//        Optional optTask = taskRepository.findById(taskId);
-//        if (optTask.isPresent()) {
-//            Task task = (Task) optTask.get();
-//            model.addAttribute("task", task);
-//
-//            return "view";
+    @GetMapping("taskDetails")
+    public String displayTaskDetails(@RequestParam(required = false) Integer taskId, Model model) {
+
+        Optional<Task> result = taskRepository.findById(taskId);
+
+        if (result.isEmpty()) {
+            model.addAttribute("title", "Invalid Task Id: " + taskId);
+        } else {
+            Task task = result.get();
+            model.addAttribute("title", task.getName() + " Details");
+            model.addAttribute("task", task);
+//            model.addAttribute("tags", "task.getTasks());
+        }
+
+        return "taskDetails/taskDetails";
+
+//        //this code works
+//        if (taskId == null) {
+//            model.addAttribute("title", "All Tasks");
+//            model.addAttribute("tasks", taskRepository.findAll());
 //        } else {
-//            return "redirect:../";
-//        }
+//            model.addAttribute("task", taskRepository.findById(taskId).get());
+
+
+//            if (taskId == null) {
+//                model.addAttribute("title", "All Tasks");
+//                model.addAttribute("tasks", taskRepository.findAll());
+//            } else {
+////            Optional<TaskCategory> result = taskCategoryRepository.findById(taskId);
+////            if (result.isEmpty()) {
+////                model.addAttribute("title", "Invalid Category Id: " + categoryId);
+////            } else {
+////                TaskCategory category = result.get();
+////                model.addAttribute("title", "Tasks in Category: " + category.getName());
+////                model.addAttribute("tasks", category.getTasks());
+////            }
+//                model.addAttribute("task", taskRepository.findById(taskId).get());
+////            model.addAttribute("tasks", category.getTasks());
+
     }
+
+//    @GetMapping("")
+//    public String displayTaskDetails() {return "taskDetails/taskDetails";}
+
+}
