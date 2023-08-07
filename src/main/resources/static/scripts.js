f56aa13be3780364f8ce593bf02df7708ffb4427
Victoria M <vmorris517@gmail.com>
2023-08-06 22:11:04 -0500

    created light and dark mode toggle for index and add page

---
+++ src/main/resources/static/scripts.js
@@ -0,0 +1,7 @@
+const switchElement = document.querySelector('.switch')
+
+document.addEventListener('click', (event) => {
+    if(event.target && event.target.matches('.switch')){
+    document.body.classList.toggle('dark')
+       }
+})
