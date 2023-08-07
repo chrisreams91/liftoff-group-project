8fe749cf48adca004808636da8038cf169bcc9c4
Shelvy <shelvy511@gmail.com>
2023-08-07 14:17:17 -0500

    update to List page Controller and list.html

---
+++ src/main/java/org/launchcode/liftoffgroupproject/models/PublishTopic.java
@@ -0,0 +1,35 @@
+package org.launchcode.liftoffgroupproject.models;
+
+
+//import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+//import software.amazon.awssdk.regions.Region;
+//import software.amazon.awssdk.services.sns.SnsClient;
+//import software.amazon.awssdk.services.sns.model.PublishRequest;
+//import software.amazon.awssdk.services.sns.model.PublishResponse;
+//import software.amazon.awssdk.services.sns.model.SnsException;
+public class PublishTopic {
+//    public static void main(String[] args) {
+//
+//        final String usage = "\n" +
+//                "Usage: " +
+//                "   <message> <topicArn>\n\n" +
+//                "Where:\n" +
+//                "   message - The message text to send.\n\n" +
+//                "   topicArn - The ARN of the topic to publish.\n\n";
+//
+//        if (args.length != 2) {
+//            System.out.println(usage);
+//            System.exit(1);
+//        }
+//
+//        String message = args[0];
+//        String topicArn = args[1];
+//        SnsClient snsClient = SnsClient.builder()
+//                .region(Region.US_EAST_1)
+//                .credentialsProvider(EnvironmentVariableCredentialsProvider.create()).build()
+//                .build();
+//        pubTopic(snsClient, message, topicArn);
+//        snsClient.close();
+//
+//    }
+}
