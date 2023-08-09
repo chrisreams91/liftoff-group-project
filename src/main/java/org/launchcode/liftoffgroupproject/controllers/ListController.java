cdd4b7b607030a0d666a2c91de42108cf1321084
Shelvy <shelvy511@gmail.com>
2023-08-08 21:43:37 -0500

    update to List page Controller and list.html

--- src/main/java/org/launchcode/liftoffgroupproject/controllers/ListController.java
+++ src/main/java/org/launchcode/liftoffgroupproject/controllers/ListController.java
@@ -1,11 +1,16 @@
 package org.launchcode.liftoffgroupproject.controllers;
 
 import org.launchcode.liftoffgroupproject.data.TaskRepository;
+import org.launchcode.liftoffgroupproject.data.UserRepository;
+import org.launchcode.liftoffgroupproject.models.PublishTopic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +19,9 @@ import java.util.List;
 @RequestMapping(value = "list")
 public class ListController {
 
+
+    @Autowired
+    UserRepository userRepository;
     @Autowired
     private TaskRepository taskRepository;
 
@@ -22,5 +30,11 @@ public class ListController {
     public String list(Model model) {
      model.addAttribute("tasks", taskRepository.findAll());
      return "list";
+    }
+
+    @PostMapping("/{email}")
+    public String email() {
+       PublishTopic.sendMessage();
+        return "redirect:/list";
     }
 }
--- src/main/java/org/launchcode/liftoffgroupproject/models/PublishTopic.java
+++ src/main/java/org/launchcode/liftoffgroupproject/models/PublishTopic.java
@@ -1,35 +1,49 @@
 package org.launchcode.liftoffgroupproject.models;
 
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.PublishResponse;
+import software.amazon.awssdk.services.sns.model.SnsException;
 
-//import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-//import software.amazon.awssdk.regions.Region;
-//import software.amazon.awssdk.services.sns.SnsClient;
-//import software.amazon.awssdk.services.sns.model.PublishRequest;
-//import software.amazon.awssdk.services.sns.model.PublishResponse;
-//import software.amazon.awssdk.services.sns.model.SnsException;
 public class PublishTopic {
-//    public static void main(String[] args) {
-//
-//        final String usage = "\n" +
-//                "Usage: " +
-//                "   <message> <topicArn>\n\n" +
-//                "Where:\n" +
-//                "   message - The message text to send.\n\n" +
-//                "   topicArn - The ARN of the topic to publish.\n\n";
-//
-//        if (args.length != 2) {
-//            System.out.println(usage);
-//            System.exit(1);
-//        }
-//
-//        String message = args[0];
-//        String topicArn = args[1];
-//        SnsClient snsClient = SnsClient.builder()
-//                .region(Region.US_EAST_1)
-//                .credentialsProvider(EnvironmentVariableCredentialsProvider.create()).build()
-//                .build();
-//        pubTopic(snsClient, message, topicArn);
-//        snsClient.close();
-//
-//    }
+
+    public static final String AWS_ACCESS_KEY_ID = System.getenv("AWS_ACCESS_KEY_ID");
+    public static final String AWS_SECRET_ACCESS_KEY = System.getenv("AWS_SECRET_ACCESS_KEY");
+
+    public static final String TOPIC = System.getenv("TOPIC");
+
+
+    public static void sendMessage() {
+
+        final String TOPIC = System.getenv("TOPIC");
+
+        String message = "Hey Shelvy";
+        String topicArn = TOPIC;
+        SnsClient snsClient = SnsClient.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
+        pubTopic(snsClient, message, topicArn);
+        snsClient.close();
+
+    }
+
+    public static void pubTopic(SnsClient snsClient, String message, String topicArn) {
+
+        try {
+            PublishRequest request = PublishRequest.builder()
+                    .message(message)
+                    .topicArn(topicArn)
+                    .build();
+
+            PublishResponse result = snsClient.publish(request);
+            System.out.println(result.messageId() + " Message sent. Status is " + result.sdkHttpResponse().statusCode());
+
+        } catch (SnsException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
 }
