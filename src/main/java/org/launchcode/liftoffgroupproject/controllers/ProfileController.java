994fe780465fccefee25250cd1dcf45f89eea659
allybreeden <allybreeden@gmail.com>
2023-08-13 12:11:30 -0500

    Added imaged to each radio and updated values to reflect filepaths, changed the profile photo to update according to user & removed code used for testing purposes, changed postmapping for picture to be able to use the current logged in user, and cleaned up code. Added profile picture images

--- src/main/java/org/launchcode/liftoffgroupproject/controllers/ProfileController.java
+++ src/main/java/org/launchcode/liftoffgroupproject/controllers/ProfileController.java
@@ -28,59 +28,31 @@ public class ProfileController {
     private AuthenticationController authenticationController;
 
 
-//    public string assignProfilePicture(String profilePicture) {
-//        if (profilePicture.equals("option1")) {
-//            return profilePicture.equals("/images/user.png");
-//        }›
-//        else if (profilePicture.equals("option2")) {
-//            return profilePicture.equals("/images/owl.png");
-//        }
-//
-//        else if (profilePicture.equals("option3")) {
-//            return profilePicture.equals("/images/panda.png");
-//        }
-//        else if (profilePicture.equals("option4")) {
-//            return profilePicture.equals("/images/zebra.png");
-//        }
-//        else {
-//            return profilePicture.equals("/images/user.png");
-//        }
-//    }
 
    @GetMapping("")
-       public String Profile(Model model, User user, Integer userId, HttpSession session) {
+       public String Profile(Model model, HttpSession session, String username, String firstName) {
 
-       Optional<User> result = userRepository.findById(authenticationController.getUserFromSession(session).getId());
-       if (result.isPresent()) {
-           User loggedUser = result.get();
-           model.addAttribute("user", loggedUser);
-           model.addAttribute("username", userRepository.findById(loggedUser.getId()));
-           model.addAttribute("first name", userRepository.findById(loggedUser.getId()));
-           model.addAttribute("first name", authenticationController.getUserFromSession(session).getFirstName());
-           model.addAttribute("username", authenticationController.getUserFromSession(session).getUsername());
-       }
+       Object userId = session.getAttribute("user");
+       Optional<User> result = userRepository.findById((Integer) userId);
+       User user = result.get();
 
-       model.addAttribute("first name", authenticationController.getUserFromSession(session).getFirstName());
-       model.addAttribute("username", authenticationController.getUserFromSession(session).getUsername());
+       model.addAttribute("tasks", user.getFirstName());
+
+//       User user = authenticationController.getUserFromSession(session);
+//
+//       model.addAttribute("first name", user.getFirstName());
+//       model.addAttribute("username", user.getUsername());
+//
+//       model.addAttribute("first name", authenticationController.getUserFromSession(session).getUsername());
+//       model.addAttribute("username", authenticationController.getUserFromSession(session).getFirstName());
+
+
 
 
        // get all users
        //this is working to get ALL users
        model.addAttribute("users", userRepository.findAll());
 
-       // don't need
-       // get username
-//       String uName = user.getUsername();
-//       model.addAttribute("username", "username");
-//       model.addAttribute("username", userRepository.findByUsername(uName));
-
-       // don't need
-//       test
-//       Optional<User> individualUser = userRepository.findById(user.getId());
-//       model.addAttribute("user", userRepository.findById(user.getId()));
-
-       //get first name
-       model.addAttribute("first name", "firstName");
 
        //list all tasks
        model.addAttribute("tasks", taskRepository.findAll());
@@ -92,31 +64,14 @@ public class ProfileController {
    @GetMapping("picture")
     public String displayChooseProfilePicture(Model model, User user) {
        model.addAttribute("profilePicture", "Edit Profile Picture");
-       model.addAttribute("profilePicture", userRepository.findByUsername(user.getUsername()));
        return "picture";
    }
 
    @PostMapping("picture")
-//    public String processChooseProfilePicture(@ModelAttribute("user") User user, String profilePicture, Model model) {
-   public String processChooseProfilePicture(User user, String profilePicture, Model model) {
-       User currentUser = userRepository.findByUsername(user.getUsername());
+   public String processChooseProfilePicture(HttpSession session, String profilePicture) {
+       User user = authenticationController.getUserFromSession(session);
        user.setProfilePicture(profilePicture);
-       userRepository.save(currentUser);
+       userRepository.save(user);
        return "redirect:/profile";
    }
-
-
-
-
-//   @ModelAttribute("profilePhotoList")
-//    public List<String> getProfilePhotoList() {
-//       List<String> profilePhotoList = new ArrayList<String>();
-//       profilePhotoList.add("/images/user.png");
-//       profilePhotoList.add("/images/owl.png");
-//       profilePhotoList.add("/images/panda.png");
-//       profilePhotoList.add("/images/zebra.png");
-//       return profilePhotoList;
-//   }
-
-
 }
