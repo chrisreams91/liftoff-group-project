03311f5c825952530fb838ede23bb57499169984
Shelvy <shelvy511@gmail.com>
2023-08-16 09:00:50 -0500

    update to List page Controller and list.html

--- src/main/java/org/launchcode/liftoffgroupproject/models/User.java
+++ src/main/java/org/launchcode/liftoffgroupproject/models/User.java
@@ -5,7 +5,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
