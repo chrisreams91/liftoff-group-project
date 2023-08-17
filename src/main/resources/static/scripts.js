9142326391981fc7e9b027876789add7c64b92c5
sjesus <s.jesus10@yahoo.com>
2023-08-17 18:22:42 -0500

    changed scripts.js 1

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,5 +1,6 @@
  const dateRadio = document.getElementById('date')
 
+    console.log("test");
   dateRadio.addEventListener('click', (event) => {
           const keyword = document.getElementById("searchTerm");
             keyword.type = "date";
