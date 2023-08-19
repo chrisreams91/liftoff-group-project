aef9ee912909d5dc85bc308d91bc71017ba61646
Victoria M <vmorris517@gmail.com>
2023-08-19 18:41:59 -0500

    made some navbar changes

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,42 +1,10 @@
-let darkTheme = localStorage.getItem('dark');
-const switchElement = document.querySelector('.switch');
-
-const enableDarkMode() => {
-document.body.classList.toggle('dark')
-localStorage.setItem('dark','enabled' )
-}
-
-const disableDarkMode = () => {
-  document.body.classList.remove('dark');
-  localStorage.setItem('dark', null);
-}
-
-if(dark === 'enabled'){
-enableDarkMode();
-}
-
-switchElement.addEventListener('click', () => {
-darkTheme = localStorage.getItem('dark');
-    if('dark' !== 'enabled'){
-    enableDarkMode();
-    } else {
-     disableDarkMode();
-    }
-   });
-
-
-
-
-
-
-/*const switchElement = document.querySelector('.switch')
-
+const switchElement = document.querySelector('.switch')
 
 document.addEventListener('click', (event) => {
     if(event.target && event.target.matches('.switch')){
     document.body.classList.toggle('dark');
        }
-});*/
+});
 
 
 
